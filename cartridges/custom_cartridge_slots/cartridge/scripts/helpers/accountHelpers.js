57e9a1655c2b8cef9bb405b3d99e3819f62642af
VelislavaC4Nexus <velislava.marinova.c4Nexus@gmail.com>
2024-01-06 19:32:58 +0200

    fix exclussive products send email, remove inline styles

--- cartridges/custom_cartridge_slots/cartridge/scripts/helpers/accountHelpers.js
+++ cartridges/custom_cartridge_slots/cartridge/scripts/helpers/accountHelpers.js
@@ -1,3 +1,41 @@
+// 'use strict';
+// var URLUtils = require('dw/web/URLUtils');
+// var base = module.superModule;
+
+// /**
+//  * Send an email that would notify the user that account was created
+//  * @param {obj} registeredUser - object that contains user's email address and name information.
+//  */
+// base.sendCreateAccountEmail = function (registeredUser) {
+//     var emailHelpers = require('*/cartridge/scripts/helpers/emailHelpers');
+//     var Site = require('dw/system/Site');
+//     var Resource = require('dw/web/Resource');
+//     var exclusiveProductHelpers = require('./exclusiveProductsHelpers');
+
+//     var userObject = {
+//         email: registeredUser.email,
+//         firstName: registeredUser.firstName,
+//         lastName: registeredUser.lastName,
+//         url: URLUtils.https('Login-Show'),
+//         allExclusiveProductIds: exclusiveProductHelpers.getAllExclusiveProductIds()
+//     };
+
+//     var emailObj = {
+//         to: registeredUser.email,
+//         subject: Resource.msg('email.subject.new.registration', 'registration', null),
+//         from: Site.current.getCustomPreferenceValue('customerServiceEmail') || 'no-reply@testorganization.com',
+//         type: emailHelpers.emailTypes.registration
+//     };
+
+//     emailHelpers.sendEmail(
+//         emailObj,
+//         'checkout/confirmation/accountRegisteredEmail',
+//         userObject
+//     );
+// }
+
+// module.exports = base;
+
 'use strict';
 var URLUtils = require('dw/web/URLUtils');
 var base = module.superModule;
@@ -6,18 +44,20 @@ var base = module.superModule;
  * Send an email that would notify the user that account was created
  * @param {obj} registeredUser - object that contains user's email address and name information.
  */
-base.sendCreateAccountEmail = function (registeredUser) {
+function sendCreateAccountEmail(registeredUser) {
     var emailHelpers = require('*/cartridge/scripts/helpers/emailHelpers');
     var Site = require('dw/system/Site');
     var Resource = require('dw/web/Resource');
-    var exclusiveProductHelpers = require('./exclusiveProductsHelpers');
-
+    // var context = require('*/cartridge/scripts/helpers/exclusiveProductHelpers');
+    var exclusiveProductsIDs = require('./exclusiveProductHelpers');
+    
     var userObject = {
         email: registeredUser.email,
         firstName: registeredUser.firstName,
         lastName: registeredUser.lastName,
-        url: URLUtils.https('Login-Show'),
-        allExclusiveProductIds: exclusiveProductHelpers.getAllExclusiveProductIds()
+        // context: context.getExclusiveProduct(),
+        exclusiveProductsIDs: exclusiveProductsIDs.getExclusiveProducts(),
+        url: URLUtils.https('Login-Show')
     };
 
     var emailObj = {
@@ -27,12 +67,9 @@ base.sendCreateAccountEmail = function (registeredUser) {
         type: emailHelpers.emailTypes.registration
     };
 
-    emailHelpers.sendEmail(
-        emailObj,
-        'checkout/confirmation/accountRegisteredEmail',
-        userObject
-    );
+    emailHelpers.sendEmail(emailObj, 'checkout/confirmation/accountRegisteredEmail', userObject);
 }
 
+base.sendCreateAccountEmail = sendCreateAccountEmail;
 module.exports = base;
 
